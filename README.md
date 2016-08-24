ea359233a1a2aaebb47296ca346e4dc2fee508d7
magl88 <magl88net@gmail.com>
2016-08-24 19:22:56 +0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # gulp-m88themes
 Theme for PSD to HTML 
+
+`npm install --save-dev gulp gulp-autoprefixer gulp-connect gulp-livereload gulp-minify-css gulp-rename gulp-sass gulp-sourcemaps`
+
+npm install --save-dev gulp
+npm install --save-dev gulp-autoprefixer
+npm install --save-dev gulp-connect
+npm install --save-dev gulp-livereload
+npm install --save-dev gulp-minify-css
+npm install --save-dev gulp-rename
+npm install --save-dev gulp-sass
+npm install --save-dev gulp-sourcemaps
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 //===========================================
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
@@ -11,42 +11,43 @@ var gulp = require('gulp'),
 //===========================================
 gulp.task('connect', function () {
 	connect.server({
-		root: 'build',
+		root: 'app',
 		livereload: true
 	})
 });
 //===========================================
 gulp.task('html', function(){
-	gulp.src('build/*.html')
+	gulp.src('app/*.html')
 		.pipe(connect.reload());
 });
 //===========================================
 gulp.task('scss', function () {
-	gulp.src('build/scss/*.scss')
+	gulp.src('app/scss/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(prefixer('last 20 version'))
+		.pipe(prefixer('last 10 version'))
 		.pipe(minifyCss())
 		.pipe(rename(function (path) {
 			path.extname = ".min.css"
 		}))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('build/css/'))
+		.pipe(gulp.dest('app/css/'))
 		.pipe(connect.reload());
 });
 gulp.task('css', function () {
-	gulp.src('build/scss/*.scss')
+	gulp.src('app/scss/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass())
-		.pipe(prefixer('last 20 version'))
-		.pipe(gulp.dest('build/css/'))
+		.pipe(prefixer('last 15 version'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('app/css/'))
 		.pipe(connect.reload());
 });
 //===========================================
 gulp.task('watch', function(){
-	gulp.watch('build/scss/**/*.scss', ['scss'])
-	gulp.watch('build/css/**/*.css', ['css'])
-	gulp.watch('build/*.html', ['html'])
+	gulp.watch('app/scss/**/*.scss', ['scss'])
+	gulp.watch('app/css/**/*.css', ['css'])
+	gulp.watch('app/*.html', ['html'])
 });
 //===========================================
 gulp.task('default', ['connect', 'html', 'scss', 'css', 'watch']);
