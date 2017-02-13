b1bb28063dc559bc3036a5564e84d3c259210dbb
magl88 <magl88net@gmail.com>
2017-02-13 11:04:12 +0200

    Update files

--- build/js/main.js
+++ build/js/main.js
@@ -2,7 +2,17 @@
 (function($){
 	$(document).ready(function(){
 		// ======================
-		console.log("Test JS");
+		for (var i in navigator) {
+			console.log(i +' : ' + navigator[i]);
+		}
+		var ua = navigator.userAgent;
+		// var reg = ua.search(/Chrome/gi)
+		// console.log(ua);
+		if (ua.search(/Chrome/gi)) {
+			console.log('chrome');
+		}else {
+			console.log('errod');
+		}
 		// ======================
 	});
 })(jQuery)
